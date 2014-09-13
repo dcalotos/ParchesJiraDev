ac0afc413309c7184cbc49be1423dbec4a9b307b
dcaloto <dcalotos@gmail.com>
2014-09-13 11:21:18 +0200

    JPD-26 Añadido el código de la clase CECCambioNivelSeguridadFactory.java

--- src/main/java/com/dc/jira/jira/workflow/CECCambioNivelSeguridadFactory.java
+++ src/main/java/com/dc/jira/jira/workflow/CECCambioNivelSeguridadFactory.java
@@ -1,46 +1,54 @@
 package com.dc.jira.jira.workflow;
 
+import com.atlassian.jira.component.ComponentAccessor;
+import com.atlassian.jira.issue.IssueManager;
 import com.atlassian.jira.plugin.workflow.AbstractWorkflowPluginFactory;
 import com.atlassian.jira.plugin.workflow.WorkflowPluginFunctionFactory;
-import com.atlassian.jira.workflow.JiraWorkflow;
+import com.atlassian.jira.project.Project;
+import com.atlassian.jira.project.ProjectManager;
+import com.atlassian.jira.security.roles.ProjectRole;
+import com.atlassian.jira.security.roles.ProjectRoleActors;
+import com.atlassian.jira.security.roles.ProjectRoleManager;
 import com.atlassian.jira.workflow.WorkflowManager;
-import com.opensymphony.workflow.loader.*;
-import webwork.action.ActionContext;
+import com.opensymphony.workflow.loader.AbstractDescriptor;
+import com.opensymphony.workflow.loader.FunctionDescriptor;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
-/*
-This is the factory class responsible for dealing with the UI for the post-function.
-This is typically where you put default values into the velocity context and where you store user input.
- */
-
+@SuppressWarnings("unused")
 public class CECCambioNivelSeguridadFactory extends AbstractWorkflowPluginFactory implements WorkflowPluginFunctionFactory{
 
-    public static final String FIELD_MESSAGE="messageField";
-    private WorkflowManager workflowManager;
+    public static final String USER_ADMIN = "userAdmin";
 
+    private ProjectManager projectManager;
+    private ProjectRoleManager projectRoleManager;
 
-    public CECCambioNivelSeguridadFactory(WorkflowManager workflowManager){
-            this.workflowManager=workflowManager;
+    public CECCambioNivelSeguridadFactory(ProjectManager projectManager, IssueManager issueManager, ProjectRoleManager projectRoleManager,
+                                          WorkflowManager workflowManager) {
+        this.projectManager = projectManager;
+        this.projectRoleManager = projectRoleManager;
+    }
+
+    @SuppressWarnings({ "rawtypes" })
+    @Override
+    protected void getVelocityParamsForInput(Map<String, Object> velocityParams) {
+        // Map<String, String[]> myParams = ActionContext.getParameters();
+        projectManager = ComponentAccessor.getProjectManager();
+        Project project = projectManager.getProjectObjByKey("CE");
+
+        projectRoleManager = ComponentAccessor.getComponentOfType(ProjectRoleManager.class);
+        ProjectRole projectRole = projectRoleManager.getProjectRole("Administrators");
+
+        ProjectRoleActors projectRoleActors = projectRoleManager.getProjectRoleActors(projectRole, project);
+        Set allUsers = projectRoleActors.getUsers();
+
+        velocityParams.put("allUsers", allUsers);
     }
 
     @Override
-    protected void getVelocityParamsForInput(Map<String, Object>velocityParams){
-
-        Map<String, String[]>myParams=ActionContext.getParameters();
-
-        final JiraWorkflow jiraWorkflow=workflowManager.getWorkflow(myParams.get("workflowName")[0]);
-
-        //the default message
-        velocityParams.put(FIELD_MESSAGE,"Workflow Last Edited By "+jiraWorkflow.getUpdateAuthorName());
-
-    }
-
-    @Override
-    protected void getVelocityParamsForEdit(Map<String, Object>velocityParams,AbstractDescriptor descriptor){
+    protected void getVelocityParamsForEdit(Map<String, Object> velocityParams, AbstractDescriptor descriptor) {
 
         getVelocityParamsForInput(velocityParams);
         getVelocityParamsForView(velocityParams, descriptor);
@@ -48,30 +56,23 @@ public class CECCambioNivelSeguridadFactory extends AbstractWorkflowPluginFactor
     }
 
     @Override
-    protected void getVelocityParamsForView(Map<String, Object>velocityParams,AbstractDescriptor descriptor){
-        if(!(descriptor instanceof FunctionDescriptor))
-        {
+    protected void getVelocityParamsForView(Map<String, Object> velocityParams, AbstractDescriptor descriptor) {
+        if (!(descriptor instanceof FunctionDescriptor)) {
             throw new IllegalArgumentException("Descriptor must be a FunctionDescriptor.");
         }
 
-        FunctionDescriptor functionDescriptor=(FunctionDescriptor)descriptor;
+        FunctionDescriptor functionDescriptor = (FunctionDescriptor) descriptor;
 
-        String message=(String)functionDescriptor.getArgs().get(FIELD_MESSAGE);
-
-        if(message==null){
-            message="No Message";
-        }
-
-        velocityParams.put(FIELD_MESSAGE,message);
+        String userAdmin = (String) functionDescriptor.getArgs().get("userAdmin");
+        velocityParams.put("userAdmin", userAdmin);
     }
 
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    public Map<String, ?> getDescriptorParams(Map<String, Object> formParams) {
+        Map params = new HashMap();
 
-    public Map<String,?>getDescriptorParams(Map<String, Object>formParams){
-        Map params=new HashMap();
-
-        // Process The map
-        String message=extractSingleParam(formParams,FIELD_MESSAGE);
-        params.put(FIELD_MESSAGE,message);
+        String userAdmin = extractSingleParam(formParams, "userAdmin");
+        params.put("userAdmin", userAdmin);
 
         return params;
     }
