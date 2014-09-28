867f0551aa56b4174ce7642e46c105850ccecc47
dcaloto <dcalotos@gmail.com>
2014-09-28 10:45:30 +0200

    JPD-44 Añadido el código de la clase CECValidadorCUC.java

--- src/main/java/com/dc/jira/jira/workflow/CECValidadorCUC.java
+++ src/main/java/com/dc/jira/jira/workflow/CECValidadorCUC.java
@@ -1,26 +1,49 @@
 package com.dc.jira.jira.workflow;
 
+import com.atlassian.jira.issue.CustomFieldManager;
+import com.atlassian.jira.issue.Issue;
+import com.atlassian.jira.issue.fields.CustomField;
+import com.opensymphony.module.propertyset.PropertySet;
+import com.opensymphony.workflow.InvalidInputException;
+import com.opensymphony.workflow.Validator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import com.atlassian.jira.issue.Issue;
-import com.opensymphony.module.propertyset.PropertySet;
-import com.opensymphony.workflow.Validator;
-import com.opensymphony.workflow.InvalidInputException;
+
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class CECValidadorCUC implements Validator
-{
+@SuppressWarnings("unused")
+public class CECValidadorCUC implements Validator {
     private static final Logger log = LoggerFactory.getLogger(CECValidadorCUC.class);
-    public static final String FIELD_WORD="word";
 
-    public void validate(Map transientVars, Map args, PropertySet ps) throws InvalidInputException
-    {
-        String word = (String) transientVars.get(FIELD_WORD);
+    private final CustomFieldManager customFieldManager;
+
+    private static final String FIELD_NAME = "field";
+
+    public CECValidadorCUC(CustomFieldManager customFieldManager) {
+        this.customFieldManager = customFieldManager;
+    }
+
+    @SuppressWarnings("rawtypes")
+    public void validate(Map transientVars, Map args, PropertySet ps) throws InvalidInputException {
+        String CUC = "";
+        Object cfValue = null;
 
         Issue issue = (Issue) transientVars.get("issue");
+        String field = (String) args.get(FIELD_NAME);
 
-        if(null == issue.getDescription() || "".equals(issue.getDescription()) || !issue.getDescription().contains(word)) {
-            throw new InvalidInputException("Issue must contain the word '" + word + "' in the description");
+        CustomField customField = customFieldManager.getCustomFieldObjectByName(field);
+        CUC = (String) issue.getCustomFieldValue(customField).toString();
+
+        if (!CUC.contains("7956627")) {
+            Pattern p = Pattern.compile("[0-9]+[-|/][0-9]+[-|/][0-9]+");
+            Matcher m = p.matcher(CUC);
+            if (!m.find()) {
+                throw new InvalidInputException(
+                        "<B>El campo CUC no tiene el formato correcto.</B><BR />"
+                                + "El formato correcto es CLIENTE-UBICACION-CONTACTO. Muy importante los guiones o barras separadoras.");
+            }
         }
     }
 }
