d98e585adf410da56acece28d7a774a6bc93e12e
dcaloto <dcalotos@gmail.com>
2014-10-05 10:09:13 +0200

    JPD-51 Añadido el código de las clases GDVAsignacionValidador"

--- src/main/java/com/dc/jira/jira/workflow/GDVAsignacionValidador.java
+++ src/main/java/com/dc/jira/jira/workflow/GDVAsignacionValidador.java
@@ -1,32 +1,102 @@
 package com.dc.jira.jira.workflow;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import com.atlassian.jira.issue.Issue;
+import com.atlassian.jira.component.ComponentAccessor;
+import com.atlassian.jira.event.type.EventDispatchOption;
+import com.atlassian.jira.issue.CustomFieldManager;
+import com.atlassian.jira.issue.IssueManager;
+import com.atlassian.jira.issue.MutableIssue;
+import com.atlassian.jira.issue.fields.CustomField;
+import com.atlassian.jira.issue.index.IssueIndexManager;
+import com.atlassian.jira.user.DelegatingApplicationUser;
+import com.atlassian.jira.user.UserUtils;
+import com.atlassian.jira.user.util.UserUtil;
+import com.atlassian.jira.util.ImportUtils;
 import com.atlassian.jira.workflow.function.issue.AbstractJiraFunctionProvider;
 import com.opensymphony.module.propertyset.PropertySet;
+import com.opensymphony.workflow.InvalidInputException;
 import com.opensymphony.workflow.WorkflowException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import util.DatosBBDD;
+
+import java.util.List;
 import java.util.Map;
-import com.atlassian.jira.issue.MutableIssue;
+import java.util.SortedSet;
 
 
-/*
-This is the post-function class that gets executed at the end of the transition.
-Any parameters that were saved in your factory class will be available in the transientVars Map.
- */
+@SuppressWarnings("unused")
+public class GDVAsignacionValidador extends AbstractJiraFunctionProvider {
+	private static final Logger log = LoggerFactory.getLogger(GDVAsignacionValidador.class);
+	public static final String USER_ADMIN = "userAdmin";
 
-public class GDVAsignacionValidador extends AbstractJiraFunctionProvider{
-    private static final Logger log = LoggerFactory.getLogger(GDVAsignacionValidador.class);
-    public static final String FIELD_MESSAGE="messageField";
+	@SuppressWarnings({ "rawtypes" })
+	public void execute(Map transientVars, Map args, PropertySet ps) throws WorkflowException {
 
-    public void execute(Map transientVars,Map args,PropertySet ps)throws WorkflowException{
-        MutableIssue issue=getIssue(transientVars);
-        String message = (String)transientVars.get(FIELD_MESSAGE);
+		MutableIssue issue = getIssue(transientVars);
+		IssueManager issueManager = ComponentAccessor.getIssueManager();
+		UserUtil uu = ComponentAccessor.getUserUtil();
+		String responsable = "";
+		log.warn("Reporter:" + issue.getReporter().getDisplayName());
+		SortedSet<String> grupos = ComponentAccessor.getUserUtil().getGroupNamesForUser(issue.getReporter().getName());
 
-        if(null == message) {
-            message = "";
-        }
+		CustomFieldManager customFieldManager = ComponentAccessor.getCustomFieldManager();
+		List<CustomField> customFields = customFieldManager.getCustomFieldObjects(issue);
 
-        issue.setDescription(issue.getDescription() + message);
-    }
+		String empleadoViajero = null;
+		DelegatingApplicationUser viajero = null;
+		if (customFields.contains(customFieldManager.getCustomFieldObjectByName("Employee who travels"))) {
+			if (customFieldManager.getCustomFieldObjectByName("Employee who travels").getValue(issue) != null) {
+				//CustomField solvedBy = customFieldManager.getCustomFieldObjectByName("Employee who travels");
+				viajero = (DelegatingApplicationUser) customFieldManager.getCustomFieldObjectByName("Employee who travels").getValue(issue);
+				empleadoViajero = viajero.getUsername();
+			} else {
+				empleadoViajero = UserUtils.getUser((String) args.get("userAdmin")).getName();
+				log.warn("Field Employee who travels contains null value.");
+			}
+		} else {
+			empleadoViajero = UserUtils.getUser((String) args.get("userAdmin")).getName();
+			log.warn("CustomField Employee who travels not exists.");
+		}
+		log.warn("Employee who travels: [" + empleadoViajero + "]");
+
+		DatosBBDD metodos = new DatosBBDD();
+		responsable = metodos.ObtencionValidador(empleadoViajero);
+		metodos = null;
+
+		if (responsable.length() > 0) {
+
+			issue.setAssignee(UserUtils.getUser(responsable));
+			// issue.setAssignee(uu.getUserByName(responsable).getDirectoryUser());
+			// issue.setAssignee(uu.getUser(responsable));
+			IssueIndexManager issueIndexManager = ComponentAccessor.getIssueIndexManager();
+			ImportUtils.setIndexIssues(false);
+			try {
+				issueIndexManager.deIndex(issue);
+			} catch (Exception ie) {
+				log.warn("Failed to deindex issue: " + issue.getKey(), ie);
+			}
+			try {
+				issueManager.updateIssue(UserUtils.getUser((String) args.get("userAdmin")), issue, EventDispatchOption.DO_NOT_DISPATCH, false);
+				// issueManager.updateIssue(uu.getUser((String)
+				// args.get("userAdmin")), issue,
+				// EventDispatchOption.DO_NOT_DISPATCH, false);
+				// issueManager.updateIssue(uu.getUserByName((String)
+				// args.get("userAdmin")).getDirectoryUser(), issue,
+				// EventDispatchOption.DO_NOT_DISPATCH, false);
+			} catch (Exception ie) {
+				log.warn("Failed to updateIssue issue: " + issue.getKey(), ie);
+			}
+			ImportUtils.setIndexIssues(true);
+			try {
+				issueIndexManager.reIndex(issue);
+			} catch (Exception e) {
+				log.warn("Failed to reindex issue: " + issue.getKey(), e);
+			}
+			log.warn("Cambio de usuario realizado.");
+		} else {
+			throw new InvalidInputException("<b>NO DISPONE DE UN RESPONSABLE DE APROBACI\u00d3N.</b><br />"
+					+ "S\u00ed lo necesita p\u00f3ngase en contacto con Nuria Fern\u00e1ndez Su\u00e1rez.");
+		}
+
+	}
 }
--- src/main/java/com/dc/jira/jira/workflow/GDVAsignacionValidadorFactory.java
+++ src/main/java/com/dc/jira/jira/workflow/GDVAsignacionValidadorFactory.java
@@ -1,79 +1,87 @@
 package com.dc.jira.jira.workflow;
 
+import com.atlassian.jira.component.ComponentAccessor;
+import com.atlassian.jira.issue.IssueManager;
 import com.atlassian.jira.plugin.workflow.AbstractWorkflowPluginFactory;
 import com.atlassian.jira.plugin.workflow.WorkflowPluginFunctionFactory;
-import com.atlassian.jira.workflow.JiraWorkflow;
+import com.atlassian.jira.project.Project;
+import com.atlassian.jira.project.ProjectManager;
+import com.atlassian.jira.security.roles.ProjectRole;
+import com.atlassian.jira.security.roles.ProjectRoleActors;
+import com.atlassian.jira.security.roles.ProjectRoleManager;
 import com.atlassian.jira.workflow.WorkflowManager;
-import com.opensymphony.workflow.loader.*;
-import webwork.action.ActionContext;
+import com.opensymphony.workflow.loader.AbstractDescriptor;
+import com.opensymphony.workflow.loader.FunctionDescriptor;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
-/*
-This is the factory class responsible for dealing with the UI for the post-function.
-This is typically where you put default values into the velocity context and where you store user input.
- */
+@SuppressWarnings("unused")
+public class GDVAsignacionValidadorFactory extends AbstractWorkflowPluginFactory  implements WorkflowPluginFunctionFactory{
 
-public class GDVAsignacionValidadorFactory extends AbstractWorkflowPluginFactory implements WorkflowPluginFunctionFactory{
+	public static final String USER_ADMIN = "userAdmin";
 
-    public static final String FIELD_MESSAGE="messageField";
-    private WorkflowManager workflowManager;
+	private ProjectManager projectManager;
+	private ProjectRoleManager projectRoleManager;
 
+	public GDVAsignacionValidadorFactory(ProjectManager projectManager, IssueManager issueManager, ProjectRoleManager projectRoleManager,
+			WorkflowManager workflowManager) {
+		this.projectManager = projectManager;
+		this.projectRoleManager = projectRoleManager;
+	}
 
-    public GDVAsignacionValidadorFactory(WorkflowManager workflowManager){
-            this.workflowManager=workflowManager;
-    }
+	@SuppressWarnings({ "rawtypes" })
+	@Override
+	protected void getVelocityParamsForInput(Map<String, Object> velocityParams) {
+		// Map<String, String[]> myParams = ActionContext.getParameters();
+		projectManager = ComponentAccessor.getProjectManager();
+		Project project = projectManager.getProjectObjByKey("GDV");
 
-    @Override
-    protected void getVelocityParamsForInput(Map<String, Object>velocityParams){
+		projectRoleManager = ComponentAccessor.getComponentOfType(ProjectRoleManager.class);
+		ProjectRole projectRole = projectRoleManager.getProjectRole("Administrators");
 
-        Map<String, String[]>myParams=ActionContext.getParameters();
+		ProjectRoleActors projectRoleActors = projectRoleManager.getProjectRoleActors(projectRole, project);
+		Set allUsers = projectRoleActors.getUsers();
 
-        final JiraWorkflow jiraWorkflow=workflowManager.getWorkflow(myParams.get("workflowName")[0]);
+		velocityParams.put("allUsers", allUsers);
+	}
 
-        //the default message
-        velocityParams.put(FIELD_MESSAGE,"Workflow Last Edited By "+jiraWorkflow.getUpdateAuthorName());
+	@Override
+	protected void getVelocityParamsForEdit(Map<String, Object> velocityParams, AbstractDescriptor descriptor) {
 
-    }
+		getVelocityParamsForInput(velocityParams);
+		getVelocityParamsForView(velocityParams, descriptor);
 
-    @Override
-    protected void getVelocityParamsForEdit(Map<String, Object>velocityParams,AbstractDescriptor descriptor){
+	}
 
-        getVelocityParamsForInput(velocityParams);
-        getVelocityParamsForView(velocityParams, descriptor);
+	/*
+	 * Este m�todo es que se utiliza para ver datos a trav�s de la macro de velocity. En este caso es un campo texto. En la macro de velocity:
+	 * &nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;Usuario administrador seleccionado para edici�n de la issue <b>$userAdmin</b>.
+	 */
+	@Override
+	protected void getVelocityParamsForView(Map<String, Object> velocityParams, AbstractDescriptor descriptor) {
+		if (!(descriptor instanceof FunctionDescriptor)) {
+			throw new IllegalArgumentException("Descriptor must be a FunctionDescriptor.");
+		}
 
-    }
+		FunctionDescriptor functionDescriptor = (FunctionDescriptor) descriptor;
 
-    @Override
-    protected void getVelocityParamsForView(Map<String, Object>velocityParams,AbstractDescriptor descriptor){
-        if(!(descriptor instanceof FunctionDescriptor))
-        {
-            throw new IllegalArgumentException("Descriptor must be a FunctionDescriptor.");
-        }
+		String userAdmin = (String) functionDescriptor.getArgs().get("userAdmin");
+		velocityParams.put("userAdmin", userAdmin);
+	}
 
-        FunctionDescriptor functionDescriptor=(FunctionDescriptor)descriptor;
+	/*
+	 * Para pasar los valores a la function-class. Clase que tiene la l�gica que hay que hacer en la postfunction. Los valores viajan en Map args, para poder
+	 * ser recuperados.
+	 */
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public Map<String, ?> getDescriptorParams(Map<String, Object> formParams) {
+		Map params = new HashMap();
 
-        String message=(String)functionDescriptor.getArgs().get(FIELD_MESSAGE);
+		String userAdmin = extractSingleParam(formParams, "userAdmin");
+		params.put("userAdmin", userAdmin);
 
-        if(message==null){
-            message="No Message";
-        }
-
-        velocityParams.put(FIELD_MESSAGE,message);
-    }
-
-
-    public Map<String,?>getDescriptorParams(Map<String, Object>formParams){
-        Map params=new HashMap();
-
-        // Process The map
-        String message=extractSingleParam(formParams,FIELD_MESSAGE);
-        params.put(FIELD_MESSAGE,message);
-
-        return params;
-    }
-
+		return params;
+	}
 }
